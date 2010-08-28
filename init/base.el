77106e4173ebc5df1cf5f6c8840f60293a0d3992
松本 太一 <taichino@em114-51-49-57.pool.e-mobile.ne.jp>
2010-08-29 08:38:00 +0900

    gnu globalの設定を追加

--- init/base.el
+++ init/base.el
@@ -89,19 +89,19 @@
 (anything-complete-shell-history-setup-key (kbd "C-o"))
 
 ;; gnu global
-;; (autoload 'gtags-mode "gtags" "" t)
-;; (setq gtags-mode-hook
-;;       '(lambda ()
-;;          (local-set-key "\M-t" 'gtags-find-tag)
-;;          (local-set-key "\M-r" 'gtags-find-rtag)
-;;          (local-set-key "\M-s" 'gtags-find-symbol)
-;;          (local-set-key "\C-t" 'gtags-pop-stack)
-;;          ))
-;; (add-hook 'c-mode-common-hook
-;;           '(lambda()
-;;              (gtags-mode 1)
-;;              (gtags-make-complete-list)
-;;              ))
+(autoload 'gtags-mode "gtags" "" t)
+(setq gtags-mode-hook
+      '(lambda ()
+         (local-set-key "\M-t" 'gtags-find-tag)
+         (local-set-key "\M-r" 'gtags-find-rtag)
+         (local-set-key "\M-s" 'gtags-find-symbol)
+         (local-set-key "\C-t" 'gtags-pop-stack)
+         ))
+(add-hook 'c-mode-common-hook
+          '(lambda()
+             (gtags-mode 1)
+             (gtags-make-complete-list)
+             ))
 
 ;; git
 (add-to-list 'load-path "~/.emacs.d/lisp/magit/share/emacs/site-lisp")
