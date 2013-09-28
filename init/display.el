32c778da6e599bb3172a1deb9d8ba17011ffeeca
Matsumoto Taichi <taichino@gmail.com>
2013-09-28 11:30:15 -0400

    set theme deeper blue

--- init/display.el
+++ init/display.el
@@ -1,15 +1,7 @@
 ;; color theme
-<<<<<<< HEAD
 (require 'color-theme)
 (color-theme-initialize)
-(load-theme 'wombat t)
-
-=======
-;; (require 'color-theme)
-;; (color-theme-initialize)
-;; (color-theme-arjen)
 (load-theme 'deeper-blue t)
->>>>>>> 290d60cd80fceb562ad1b0f908360cf899479e6a
 
 ;; windowのtoggle
 (defun window-toggle-division ()
