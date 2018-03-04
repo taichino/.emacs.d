e3f6d8297abee1894ac466c44f41d1a265ef31c0
Taichi Matsumoto <taichino@gmail.com>
2018-03-04 00:39:46 -0500

    remove unused configs

--- init.el
+++ init.el
@@ -1,9 +1,4 @@
 (add-to-list 'load-path "~/.emacs.d")
-(add-to-list 'load-path "~/.emacs.d/lib/lisp")
-(add-to-list 'load-path "~/.emacs.d/lib/lisp/apel")
-(add-to-list 'load-path "~/.emacs.d/lib/lisp/emu")
-(add-to-list 'load-path "~/.emacs.d/lib/lisp/howm")
-(add-to-list 'load-path "~/.emacs.d/site-lisp/ess/lisp")
 
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
@@ -37,15 +32,3 @@
 (load "init/programming.el")
 (load "init/self.el")
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (wombat))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
--- init/self.el
+++
@@ -1,6 +0,0 @@
-;; Xcode関連
-(defun open-in-xcode ()
-  (interactive)
-  (setq filename (buffer-file-name (current-buffer)))
-  (setq offset (format "%d" (point)))
-  (call-process "open_in_xcode.py" nil t nil filename offset))
