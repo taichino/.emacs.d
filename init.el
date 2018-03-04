4cfa1f51059ad07d2d58b79b00af487863d7b59f
Taichi Matsumoto <taichino@gmail.com>
2018-03-04 17:23:35 -0500

    use-package

--- init.el
+++ init.el
@@ -1,10 +1,15 @@
 (add-to-list 'load-path "~/.emacs.d/inits")
 
 (require 'package)
+(setq package-enable-at-startup nil)
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
 (package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
 
 (add-to-list 'exec-path "/usr/local/bin")
 
@@ -13,3 +18,16 @@
 (load "keybind.el")
 (load "memo.el")
 (load "programming.el")
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
