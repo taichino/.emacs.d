66a0a58fe085440457fa2ad96f6de1c3d55d25af
Taichi Matsumoto <taichino@gmail.com>
2018-03-04 17:09:48 -0500

    clean up

--- init.el
+++ init.el
@@ -3,26 +3,9 @@
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
 (package-initialize)
 
-(defvar is-mac (or (eq window-system 'mac) (featurep 'ns)))
-
-(set-language-environment  'utf-8)
-(prefer-coding-system 'utf-8)
-(set-default-coding-systems 'utf-8-unix)
-(cond
- (is-mac
-  (require 'ucs-normalize)
-  (setq file-name-coding-system 'utf-8-hfs)
-  (setq locale-coding-system 'utf-8-hfs)
-  )
- (t
-  (setq file-name-coding-system 'utf-8)
-  (setq locale-coding-system 'utf-8)
-  )
- )
-
-;; 実行パス
 (add-to-list 'exec-path "/usr/local/bin")
 
 (load "base.el")
