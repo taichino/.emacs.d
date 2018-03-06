8f313fe92fcf398b22f8ed54591c30209030e747
Taichi Matsumoto <taichino@gmail.com>
2018-03-05 23:59:34 -0500

    swap ctrl-j and ctrl-m

--- inits/keybind.el
+++ inits/keybind.el
@@ -13,38 +13,4 @@
 (global-set-key "\C-cso" 'occur-by-moccur)
 (global-set-key "\C-z" nil) 
 
-;; 対応する括弧へjump
-(progn
-  (defvar com-point nil
-    "Remember com point as a marker. \(buffer specific\)")
-  (set-default 'com-point (make-marker))
-  (defun getcom (arg)
-    "Get com part of prefix-argument ARG."
-    (cond ((null arg) nil)
-          ((consp arg) (cdr arg))
-          (t nil)))
-  (defun paren-match (arg)
-    "Go to the matching parenthesis."
-    (interactive "P")
-    (let ((com (getcom arg)))
-      (if (numberp arg)
-          (if (or (> arg 99) (< arg 1))
-              (error "Prefix must be between 1 and 99.")
-            (goto-char
-             (if (> (point-max) 80000)
-                 (* (/ (point-max) 100) arg)
-               (/ (* (point-max) arg) 100)))
-            (back-to-indentation))
-        (cond ((looking-at "[\(\[{]")
-               (if com (move-marker com-point (point)))
-               (forward-sexp 1)
-               (if com
-                   (paren-match nil com)
-                 (backward-char)))
-              ((looking-at "[])]}")
-               (forward-char)
-               (if com (move-marker com-point (point)))
-               (backward-sexp 1)
-               (if com (paren-match nil com)))
-              (t (error ""))))))
-  (define-key ctl-x-map "%" 'paren-match))
+(when (fboundp 'electric-indent-mode) (electric-indent-mode -1))
